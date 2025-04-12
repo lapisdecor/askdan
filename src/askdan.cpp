67846c28a25eaade8c56784eb902a83d3dd3922a
Luis Louro <lapisdecor@gmail.com>
2025-04-13 00:37:34 +0100

    Add csv

--- src/askdan.cpp
+++ src/askdan.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
+#include <string>
+#include "csv.h"
 
+int search(std::string package) {
+	std::cout << "You searched for " << package << std::endl;
+
+	// TODO Search pakage_files.csv
+	return 0;
+}
 
 int main(int argc, char* argv[]) {
 	if (argc == 1) {
@@ -7,6 +15,10 @@ int main(int argc, char* argv[]) {
 	}
 	if (argc > 2) {
 		std::cout << "Only one argument is allowed at the moment." << std::endl;
+	}
+	if (argc == 2) {
+		// search package in csv
+		search((std::string)argv[1]);
 	}
 	return 0;
 }
