2ba8c155c9206b6c5e6d61881c07b8346b698f1c
Luis Louro <lapisdecor@gmail.com>
2025-04-23 06:16:53 +0100

    Add recomendation

--- src/askdan.cpp
+++ src/askdan.cpp
@@ -3,21 +3,34 @@
 #include <vector>
 #include "rapidcsv.h"
 
-int search(std::string package) {
-	std::cout << "You searched for " << package << std::endl;
+void recommend(int i, rapidcsv::Document doc) {
+	int j = 0;
+	std::string arr[4] = {".deb", "Snap", "Flatpak", "AppImage"};
+	for (int j = 0; j < 4; j++) { 
+		if (doc.GetCell<std::string>(j + 1, i) == "1") {
+			std::cout << arr[j] << " found" << std::endl;
+		}
+	}
+	std::cout << "\n";
+	std::cout << arr[stoi(doc.GetCell<std::string>(5, i))] << " recommended by Dan." << std::endl;
+	std::cout << "\n";
+	std::cout << "Disclaimer: this recomendation is made for ubuntu.\n";
+	std::cout << "If you don't agree with the recomendation explain us why at askdan issues on github.\n";
+	std::cout << "If you are the developer of the software please state your prefered format.\n";
+}
 
-	// TODO Search pakage_files.csv
+int search(std::string package) {
+	//std::cout << "You searched for " << package << std::endl;
+
+	// Search pakage_files.csv
 	rapidcsv::Document doc("package_list.csv");
 
-	//std::vector<std::string> row = doc.GetRow<std::string>(package);
-	//std::cout << "Package Found!" << std::endl;
-
 	// Get columns names
-	std::cout << "Columns: ";
-	for (const auto& col : doc.GetColumnNames()) {
-		std::cout << col << " ";
-	}
-	std::cout << "\n\n";
+	//std::cout << "Columns: ";
+	//for (const auto& col : doc.GetColumnNames()) {
+	//	std::cout << col << " ";
+	//}
+	//std::cout << "\n\n";
 
 	// Get firt column
 	if (doc.GetRowCount() > 0) {
@@ -26,14 +39,15 @@ int search(std::string package) {
 		for (size_t i = 0; i < doc.GetRowCount(); ++i) {
 			//std::cout << doc.GetCell<std::string>(j, i) << "\n";
 			if (doc.GetCell<std::string>(j,i) == package) {
-				std::cout << "Found " << package << "!"  << std::endl;
+				//std::cout << "Found " << package << "!"  << std::endl;
 				found = true;
+				recommend(i, doc);
 				break;
 			}
 		}
 		std::cout << "\n";
 		if (!found) {
-			std::cout << "Package " << package << " not yet on the csv." << std::endl;
+			std::cout << "Application " << package << " not yet on the csv. You can add it." << std::endl;
 		}
 	} 
 	
@@ -42,7 +56,7 @@ int search(std::string package) {
 
 int main(int argc, char* argv[]) {
 	if (argc == 1) {
-		std::cout << "Help: type 'askdan package' to search for a package." << std::endl;
+		std::cout << "Help: type 'askdan app_name' to search for an application." << std::endl;
 	}
 	if (argc > 2) {
 		std::cout << "Only one argument is allowed at the moment." << std::endl;
